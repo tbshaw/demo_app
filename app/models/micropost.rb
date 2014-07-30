18f228b1ba2bce1af93fa0db4e10fb44d91f43c3
tbshaw <shaw07030@yahoo.com>
2014-07-29 23:13:58 -0400

    updating files for 4.0

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  #attr_accessible :content, :user_id  In Rails 4, apparently we don't use attr_accessible anymore
   
   belongs_to :user
   
